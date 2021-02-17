f22403ffe9c974e2a10ee31ba97eb77c80da1be0
이재훈 <jami8291215@gmail.com>
2021-02-17 09:53:56 +0900

    first commit

---
+++ src/view/Landing.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import landing from "../img/landing3.jpeg";
+
+
+// css
+import "../css/Landing.scss"
+
+function Landing() {
+  return (
+    // <div id="landing_img" style={{ backgroundImage: `url(${landing}` }}>
+    <div className="landing_img">
+      {/* <img src={landing} alt="랜딩페이지 배경화면입니다." /> */}
+    </div>
+  )
+}
+
+export default Landing;
---
+++ src/view/Main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Landing from "./Landing";
+
+// components
+
+
+
+
+function Main() {
+  return (
+    <div>
+      <section>
+        <Landing />
+      </section>
+    </div>
+  )
+
+}
+
+export default Main;
