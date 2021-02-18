aacec86c229b9aace455fe7131c491980c5c8e0d
이재훈 <jami8291215@gmail.com>
2021-02-18 15:16:36 +0900

    :zap: post list 반복 출력

--- src/components/BlogItemContainer.js
+++ src/components/BlogItemContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { blogDatas } from '../asset/blogData'
+import BlogItem from './BlogItem';
 
 function BlogItemContainer() {
-
+  const posts = blogDatas.channel.item;
   console.log(blogDatas)
   return (
-    <div>
-      dkk
+    <div id="item-list-container">
+      {posts.map((post, idx) => <BlogItem key={idx} post={post} />)}
     </div>
   )
 }
