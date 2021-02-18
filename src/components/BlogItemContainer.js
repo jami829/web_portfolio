359abea20a322af6499a6f2a2a55ecb149dd317e
이재훈 <jami8291215@gmail.com>
2021-02-19 02:36:43 +0900

    :zap: 블로그 포스트 10개 -> 9개로 수정

--- src/components/BlogItemContainer.js
+++ src/components/BlogItemContainer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { blogDatas } from '../asset/blogData'
 import BlogItem from './BlogItem';
 
-// css
-import '../css/BlogItemContainer.scss'
+// css -> blog.scss
+
 
 function BlogItemContainer() {
   const posts = blogDatas.channel.item;
@@ -12,7 +12,7 @@ function BlogItemContainer() {
     <div id="item-list-container">
       <div id="item-list-inner">
         {posts.map((post, idx) => {
-          if (idx < 10) {
+          if (idx < 9) {
             return <BlogItem key={idx} post={post} />
           }
           return
