763c86f01ae289704e6b14b422b1bf386858d3bf
이재훈 <jami8291215@gmail.com>
2021-02-19 00:20:38 +0900

    :art: xml에서 img url 출력

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+
+// css
+import "../css/BlogItem.scss";
 
 const BlogItem = ({ post }) => {
-  console.log(post.description)
+
+  const [srcIdx, setSrcIdx] = useState("")
+  const [imgIdx, setImgIdx] = useState("")
+  const [imgUrl, setImgUrl] = useState("")
+
+  const description = post.description;
+
+  // const getSrcIdx = () => {
+  //   if (description.includes("src")) {
+  //     setSrcIdx(
+  //       description.indexOf("src")
+  //     )
+  //     console.log("srcIdx", srcIdx)
+  //   }
+  //   else {
+  //     console.log("no img")
+  //   }
+  // }
+
+  // const getImgIdx = () => {
+  //   if (description.includes("png") || description.includes("jpg") || description.includes("jpeg")) {
+  //     setImgIdx(
+
+  //       description.indexOf("png") || description.indexOf("jpg") || description.indexOf("jpeg")
+  //     )
+  //     console.log("imgIdx", imgIdx)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getSrcIdx()
+  //   getImgIdx()
+  // })
+
+
+  const getImgUrl = () => {
+    if (description.includes("src")) {
+      setSrcIdx(
+        description.indexOf("src")
+      )
+      if (description.includes("png") || description.includes("jpg") || description.includes("jpeg")) {
+        setImgIdx(
+          description.indexOf("png") || description.indexOf("jpg") || description.indexOf("jpeg") || description.indexOf("gif")
+        )
+        setImgUrl(
+          description.substring(srcIdx + 5, imgIdx + 3)
+        )
+      }
+    }
+    else {
+      setImgUrl(
+        "no img"
+      )
+    }
+  }
+
+  useEffect(() => {
+    getImgUrl()
+    console.log("imgUrl", imgUrl)
+  })
+
+
+
+  // console.log(post.description)
   return (
+
 
     <div className="post">
       <div className="post-img">
         backgroundimg로 해결하기
+        
       </div>
       <div className="post-info">
         <strong className="post-title">타이틀</strong>
