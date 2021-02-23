a59d28fc06abd9dc9b0ee7ede59bd862b4039603
이재훈 <jami8291215@gmail.com>
2021-02-23 13:18:12 +0900

    :art: 포트폴리오 슬라이드넘버

--- src/view/Project.js
+++ src/view/Project.js
@@ -4,13 +4,15 @@ import React, { useEffect, useRef, useState } from "react";
 import "../css/Project.scss";
 
 // img
-import finalPro from "../img/final-pro2.png"
+import finalPro from "../img/final-pro.png"
 import firstPro from "../img/first-pro.png"
+
+// icons
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const TOTAL_SLIDES = 1;
 
 function Project() {
-
 
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideRef = useRef(null);
@@ -27,7 +29,7 @@ function Project() {
     // current.style.transition = "all .5s ease-in-out"
     // current.style.transform = `translateX(${currentSlide}%)`
 
-    setTimeout(goRight, 2000)
+    setTimeout(goRight, 5000)
   }, [currentSlide])
 
   return (
@@ -42,7 +44,6 @@ function Project() {
           </div>
         </div>
         <div className="project-box-2" ref={slideRef}>
-          {currentSlide}
           <div className="img-box">
             <div style={{ transform: `translateX(${currentSlide}%)` }}>
               <img src={finalPro}></img>
@@ -51,8 +52,19 @@ function Project() {
               <img src={firstPro}></img>
             </div>
           </div>
-          <button onClick={goLeft}>left</button>
-          <button onClick={goRight}>right</button>
+          <div className="moveDirect-pro">
+            {currentSlide === 0
+              ? <div className="here-pro">01</div>
+              : <div className="not-here-pro">01</div>
+            }
+            {currentSlide === -100
+              ? <div className="here-pro">02</div>
+              : <div className="not-here-pro">02</div>
+            }
+
+          </div>
+          <button id="left-btn" onClick={goLeft}><FaAngleLeft /></button>
+          <button id="right-btn" onClick={goRight}><FaAngleRight /></button>
         </div>
       </div>
     </div>
