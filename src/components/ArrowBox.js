b54e44bed00629f1631259343e8d72cfb13ca4da
이재훈 <jami8291215@gmail.com>
2021-02-24 10:53:06 +0900

    :art: 주요 스택 말풍선 및 프사 설정 완

--- src/components/ArrowBox.js
+++ src/components/ArrowBox.js
@@ -3,10 +3,15 @@ import React from 'react';
 // css
 import "../css/ArrowBox.scss";
 
-function ArrowBox({ contents }) {
+function ArrowBox({ img, contents }) {
   return (
-    <div className="arrow_box">
-      { contents}
+    <div className="arrow">
+      <div className="profile_photo">
+        <img src={img}></img>
+      </div>
+      <div className="arrow_box">
+        {contents}
+      </div>
     </div>
   )
 }
--- src/view/Stack2.js
+++ src/view/Stack2.js
@@ -5,16 +5,34 @@ import ArrowBox from '../components/ArrowBox';
 // css
 import "../css/Stack2.scss";
 
+// img 
+import cloudFront from "../img/stack/cloudFront.png"
+import css from "../img/stack/css.png"
+import ec2 from "../img/stack/ec2.png"
+import express from "../img/stack/express.png"
+import html from "../img/stack/html.png"
+import js from "../img/stack/js.png"
+import mysql from "../img/stack/mysql.png"
+import nodejs from "../img/stack/nodejs.png"
+import react from "../img/stack/react.png"
+import redux from "../img/stack/redux.png"
+import s3 from "../img/stack/s3.png"
+import scss from "../img/stack/scss.png"
+import ts from "../img/stack/type.png"
+import vue from "../img/stack/vue.png"
+
 function Stack2() {
   return (
     <div id="stack">
+      <div className="arrowbox-container">``
 
-      <ArrowBox contents="안녕하세요" />
-      <ArrowBox />
-      <ArrowBox />
-      <ArrowBox />
-      <ArrowBox />
+        <ArrowBox img={react} contents="안녕하세요" />
+        <ArrowBox img={redux} contents="redux" />
+        <ArrowBox img={vue} contents="vue" />
+        {/* <ArrowBox img={} contents="vuex" /> */}
+        <ArrowBox img={ts} contents="타입스크립트" />
 
+      </div>
     </div>
   )
 }
